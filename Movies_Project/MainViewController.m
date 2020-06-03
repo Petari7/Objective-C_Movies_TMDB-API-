4a5f4643fb13d6960df8f398bcacb441e6cd52fd
Petari7 <ivic.petar77@gmail.com>
2020-06-03 17:30:01 +0200

    Set MainViewController as root controller. Bypassed Storyboards.

--- Movies_Project/MainViewController.m
+++ Movies_Project/MainViewController.m
@@ -6,17 +6,19 @@
 //  Copyright © 2020 Petar Ivic. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "MainViewController.h"
 
-@interface ViewController ()
+@interface MainViewController ()
 
 @end
 
-@implementation ViewController
+@implementation MainViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+  
+    
+    
 }
 
 
